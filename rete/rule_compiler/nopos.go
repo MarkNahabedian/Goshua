c652cdd72fcef99a080b86084ca42bc884dfcb73
MarkNahabedian <naha@mit.edu>
2018-08-28 11:59:32 -0400

    Make NoPos work on []ast.Decl.  go fmt.

--- rete/rule_compiler/nopos.go
+++ rete/rule_compiler/nopos.go
@@ -4,15 +4,25 @@ import "fmt"
 import "go/ast"
 import "reflect"
 
-
 // NoPos strips all position information from the ast.
 func NoPos(n interface{}) interface{} {
+	var nodeType reflect.Type
+	if t := reflect.TypeOf(n); t.Kind() == reflect.Slice {
+		nodeType = t.Elem()
+	} else {
+		if node, ok := n.(ast.Node); !ok {
+			panic(fmt.Sprintf("NoPos called on %T, which is not an ast.Node", n))
+		} else {
+			nodeType = reflect.TypeOf(node)
+		}
+	}
+
 	// Parameter and result types are not ast.Node because NoPos also
 	// takes slicews.
 	//
 	// I've observed that the ast data structure can be circular, in particular
 	// ast.Ident.
-	converted := make(map[interface{}] interface{})
+	converted := make(map[interface{}]interface{})
 
 	note := func(n interface{}, c interface{}) interface{} {
 		if c1, ok := converted[n]; ok {
@@ -52,14 +62,14 @@ func NoPos(n interface{}) interface{} {
 			}
 		case *ast.BasicLit:
 			return &ast.BasicLit{
-				Kind: n1.Kind,
+				Kind:  n1.Kind,
 				Value: n1.Value,
 			}
 		case *ast.BinaryExpr:
 			return &ast.BinaryExpr{
-				X: nopos(n1.X).(ast.Expr),
+				X:  nopos(n1.X).(ast.Expr),
 				Op: n1.Op,
-				Y: nopos(n1.Y).(ast.Expr),
+				Y:  nopos(n1.Y).(ast.Expr),
 			}
 		case *ast.BlockStmt:
 			return &ast.BlockStmt{
@@ -67,12 +77,12 @@ func NoPos(n interface{}) interface{} {
 			}
 		case *ast.BranchStmt:
 			return &ast.BranchStmt{
-				Tok: n1.Tok,
+				Tok:   n1.Tok,
 				Label: nopos(n1.Label).(*ast.Ident),
 			}
 		case *ast.CallExpr:
 			return &ast.CallExpr{
-				Fun: nopos(n1.Fun).(ast.Expr),
+				Fun:  nopos(n1.Fun).(ast.Expr),
 				Args: nopos(n1.Args).([]ast.Expr),
 			}
 		case *ast.CaseClause:
@@ -82,7 +92,7 @@ func NoPos(n interface{}) interface{} {
 			}
 		case *ast.ChanType:
 			return &ast.ChanType{
-				Dir: n1.Dir,
+				Dir:   n1.Dir,
 				Value: nopos(n1.Value).(ast.Expr),
 			}
 		case *ast.CommClause:
@@ -103,12 +113,16 @@ func NoPos(n interface{}) interface{} {
 				Type: nopos(n1.Type).(ast.Expr),
 				Elts: nopos(n1.Elts).([]ast.Expr),
 			}
+		case *ast.DeclStmt:
+			return &ast.DeclStmt{
+				Decl: nopos(n1.Decl).(ast.Decl),
+			}
 		case *ast.DeferStmt:
 			return &ast.DeferStmt{
 				Call: nopos(n1.Call).(*ast.CallExpr),
 			}
-		case *ast.Ellipsis :
-			return &ast.Ellipsis {
+		case *ast.Ellipsis:
+			return &ast.Ellipsis{
 				Elt: nopos(n1.Elt).(ast.Expr),
 			}
 		case *ast.EmptyStmt:
@@ -121,10 +135,10 @@ func NoPos(n interface{}) interface{} {
 			}
 		case *ast.Field:
 			return &ast.Field{
-				Doc: nopos(n1.Doc).(*ast.CommentGroup),
-				Names: nopos(n1.Names).([]*ast.Ident),
-				Type: nopos(n1.Type).(ast.Expr),
-				Tag: nopos(n1.Tag).(*ast.BasicLit),
+				Doc:     nopos(n1.Doc).(*ast.CommentGroup),
+				Names:   nopos(n1.Names).([]*ast.Ident),
+				Type:    nopos(n1.Type).(ast.Expr),
+				Tag:     nopos(n1.Tag).(*ast.BasicLit),
 				Comment: nopos(n1.Comment).(*ast.CommentGroup),
 			}
 		case *ast.FieldList:
@@ -132,7 +146,7 @@ func NoPos(n interface{}) interface{} {
 				List: nopos(n1.List).([]*ast.Field),
 			}
 		case *ast.ForStmt:
-			return &ast.ForStmt {
+			return &ast.ForStmt{
 				Init: nopos(n1.Init).(ast.Stmt),
 				Cond: nopos(n1.Cond).(ast.Expr),
 				Post: nopos(n1.Post).(ast.Stmt),
@@ -140,7 +154,7 @@ func NoPos(n interface{}) interface{} {
 			}
 		case *ast.FuncDecl:
 			return &ast.FuncDecl{
-				Doc: nopos(n1.Doc).(*ast.CommentGroup),
+				Doc:  nopos(n1.Doc).(*ast.CommentGroup),
 				Recv: nopos(n1.Recv).(*ast.FieldList),
 				Name: nopos(n1.Name).(*ast.Ident),
 				Type: nopos(n1.Type).(*ast.FuncType),
@@ -153,13 +167,13 @@ func NoPos(n interface{}) interface{} {
 			}
 		case *ast.FuncType:
 			return &ast.FuncType{
-				Params: nopos(n1.Params).(*ast.FieldList),
+				Params:  nopos(n1.Params).(*ast.FieldList),
 				Results: nopos(n1.Results).(*ast.FieldList),
 			}
 		case *ast.GenDecl:
-			return &ast.GenDecl {
-				Doc: nopos(n1.Doc).(*ast.CommentGroup),
-				Tok: n1.Tok,
+			return &ast.GenDecl{
+				Doc:   nopos(n1.Doc).(*ast.CommentGroup),
+				Tok:   n1.Tok,
 				Specs: nopos(n1.Specs).([]ast.Spec),
 			}
 		case *ast.GoStmt:
@@ -170,7 +184,7 @@ func NoPos(n interface{}) interface{} {
 			if c, ok := converted[n]; ok {
 				return c
 			}
-			c := &ast.Ident{ Name: n1.Name }
+			c := &ast.Ident{Name: n1.Name}
 			note(n, c)
 			c.Obj = nopos(n1.Obj).(*ast.Object)
 			return c
@@ -181,34 +195,34 @@ func NoPos(n interface{}) interface{} {
 				Body: nopos(n1.Body).(*ast.BlockStmt),
 				Else: nopos(n1.Else).(ast.Stmt),
 			}
-		case *ast.IncDecStmt :
+		case *ast.IncDecStmt:
 			return &ast.IncDecStmt{
-				X: nopos(n1.X).(ast.Expr),
+				X:   nopos(n1.X).(ast.Expr),
 				Tok: n1.Tok,
 			}
 		case *ast.IndexExpr:
 			return &ast.IndexExpr{
-				X: nopos(n1.X).(ast.Expr),
+				X:     nopos(n1.X).(ast.Expr),
 				Index: nopos(n1.Index).(ast.Expr),
 			}
 		case *ast.InterfaceType:
 			return &ast.InterfaceType{
-				Methods: nopos(n1.Methods).(*ast.FieldList),
+				Methods:    nopos(n1.Methods).(*ast.FieldList),
 				Incomplete: n1.Incomplete,
 			}
 		case *ast.KeyValueExpr:
 			return &ast.KeyValueExpr{
-				Key: nopos(n1.Key).(ast.Expr),
+				Key:   nopos(n1.Key).(ast.Expr),
 				Value: nopos(n1.Value).(ast.Expr),
 			}
 		case *ast.LabeledStmt:
 			return &ast.LabeledStmt{
 				Label: nopos(n1.Label).(*ast.Ident),
-				Stmt: nopos(n1.Stmt).(ast.Stmt),
+				Stmt:  nopos(n1.Stmt).(ast.Stmt),
 			}
 		case *ast.MapType:
 			return &ast.MapType{
-				Key: nopos(n1.Key).(ast.Expr).(ast.Expr),
+				Key:   nopos(n1.Key).(ast.Expr).(ast.Expr),
 				Value: nopos(n1.Value).(ast.Expr).(ast.Expr),
 			}
 		case *ast.Object:
@@ -219,14 +233,14 @@ func NoPos(n interface{}) interface{} {
 				Data: nopos(n1.Data),
 				Type: nopos(n1.Type),
 			}
-	
+
 		case *ast.RangeStmt:
 			return &ast.RangeStmt{
-				Key: nopos(n1.Key).(ast.Expr),
+				Key:   nopos(n1.Key).(ast.Expr),
 				Value: nopos(n1.Value).(ast.Expr),
-				Tok: n1.Tok,
-				X: nopos(n1.X).(ast.Expr),
-				Body: nopos(n1.Body).(*ast.BlockStmt),
+				Tok:   n1.Tok,
+				X:     nopos(n1.X).(ast.Expr),
+				Body:  nopos(n1.Body).(*ast.BlockStmt),
 			}
 		case *ast.ReturnStmt:
 			return &ast.ReturnStmt{
@@ -238,20 +252,20 @@ func NoPos(n interface{}) interface{} {
 			}
 		case *ast.SelectorExpr:
 			return &ast.SelectorExpr{
-				X: nopos(n1.X).(ast.Expr),
+				X:   nopos(n1.X).(ast.Expr),
 				Sel: nopos(n1.Sel).(*ast.Ident),
 			}
 		case *ast.SendStmt:
 			return &ast.SendStmt{
-				Chan: nopos(n1.Chan).(ast.Expr),
+				Chan:  nopos(n1.Chan).(ast.Expr),
 				Value: nopos(n1.Value).(ast.Expr),
 			}
 		case *ast.SliceExpr:
 			return &ast.SliceExpr{
-				X: nopos(n1.X).(ast.Expr),
-				Low: nopos(n1.Low).(ast.Expr),
-				High: nopos(n1.High).(ast.Expr),
-				Max: nopos(n1.Max).(ast.Expr),
+				X:      nopos(n1.X).(ast.Expr),
+				Low:    nopos(n1.Low).(ast.Expr),
+				High:   nopos(n1.High).(ast.Expr),
+				Max:    nopos(n1.Max).(ast.Expr),
 				Slice3: n1.Slice3,
 			}
 		case *ast.StarExpr:
@@ -265,38 +279,38 @@ func NoPos(n interface{}) interface{} {
 		case *ast.SwitchStmt:
 			return &ast.SwitchStmt{
 				Init: nopos(n1.Init).(ast.Stmt),
-				Tag: nopos(n1.Tag).(ast.Expr),
+				Tag:  nopos(n1.Tag).(ast.Expr),
 				Body: nopos(n1.Body).(*ast.BlockStmt),
 			}
 		case *ast.TypeAssertExpr:
 			return &ast.TypeAssertExpr{
-				X: nopos(n1.X).(ast.Expr),
+				X:    nopos(n1.X).(ast.Expr),
 				Type: nopos(n1.Type).(ast.Expr),
 			}
 		case *ast.TypeSpec:
 			return &ast.TypeSpec{
-				Doc: nopos(n1.Doc).(*ast.CommentGroup),
-				Name: nopos(n1.Name).(*ast.Ident),
-				Type: nopos(n1.Type).(ast.Expr),
+				Doc:     nopos(n1.Doc).(*ast.CommentGroup),
+				Name:    nopos(n1.Name).(*ast.Ident),
+				Type:    nopos(n1.Type).(ast.Expr),
 				Comment: nopos(n1.Comment).(*ast.CommentGroup),
 			}
 		case *ast.TypeSwitchStmt:
 			return &ast.TypeSwitchStmt{
-				Init: nopos(n1.Init).(ast.Stmt),
+				Init:   nopos(n1.Init).(ast.Stmt),
 				Assign: nopos(n1.Assign).(ast.Stmt),
-				Body: nopos(n1.Body).(*ast.BlockStmt),
+				Body:   nopos(n1.Body).(*ast.BlockStmt),
 			}
 		case *ast.UnaryExpr:
 			return &ast.UnaryExpr{
 				Op: n1.Op,
-				X: nopos(n1.X).(ast.Expr),
+				X:  nopos(n1.X).(ast.Expr),
 			}
 		case *ast.ValueSpec:
 			return &ast.ValueSpec{
-				Doc: nopos(n1.Doc).(*ast.CommentGroup),
+				Doc:   nopos(n1.Doc).(*ast.CommentGroup),
 				Names: nopos(n1.Names).([]*ast.Ident),
 			}
-	
+
 		case []*ast.Comment:
 			r := make([]*ast.Comment, len(n1))
 			for i, c := range n1 {
@@ -315,7 +329,13 @@ func NoPos(n interface{}) interface{} {
 				r[i] = nopos(e).(*ast.Ident)
 			}
 			return r
-	
+
+		case []ast.Decl:
+			r := make([]ast.Decl, len(n1))
+			for i, d := range n1 {
+				r[i] = nopos(d).(ast.Decl)
+			}
+			return r
 		case []ast.Expr:
 			r := make([]ast.Expr, len(n1))
 			for i, e := range n1 {
@@ -334,9 +354,12 @@ func NoPos(n interface{}) interface{} {
 				r[i] = nopos(s).(ast.Stmt)
 			}
 			return r
-	
+
 		default:
-			panic(fmt.Sprintf("nopos does not support type %T.", n))
+			if reflect.TypeOf(n).AssignableTo(nodeType) {
+				panic(fmt.Sprintf("NoPos does not support type %T.", n))
+			}
+			return n
 		}
 	}
 
