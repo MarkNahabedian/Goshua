62221e67c3e7307b73c11893646530cda4e2559b
MarkNahabedian <naha@mit.edu>
2020-10-30 11:31:17 -0400

    Give each thing an id and String method to make diagnostic output easier to read.

--- rete/example/thing_rules.go
+++ rete/example/thing_rules.go
@@ -6,9 +6,19 @@ import "goshua/rete/rule_compiler/runtime"
 
 
 type thing1 struct {
+	Id string
+}
+
+func (thing *thing1) String() string {
+	return "thing1-" + thing.Id
 }
 
 type thing2 struct {
+	Id string
+}
+
+func (thing *thing2) String() string {
+	return "thing2-" + thing.Id
 }
 
 type thing3 struct {
@@ -17,6 +27,12 @@ type thing3 struct {
 	t2b *thing2
 }
 
+func (thing *thing3) String() string {
+	return "thing3" + thing.t1.Id + thing.t2a.Id + thing.t2b.Id
+}
+
+
+/*
 func foo() *thing3 {
 	return &(thing3 {
 		t1: &thing1{},
@@ -24,6 +40,7 @@ func foo() *thing3 {
 		t2b: &thing2{},
 	})
 }
+*/
 
 
 func rule_thing3(node runtime.Node, t1 *thing1, t2a, t2b *thing2) {
